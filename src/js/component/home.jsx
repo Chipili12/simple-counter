7a725c6ad1bf3c9858af78099988186a3379b1a1
Santiago Burgueño <santibur@live.com>
2023-01-09 15:22:34 +0000

    Added

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,12 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
-const Home = () => {
+const Home = (props) => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<h1 className="bg-black text-light h-100">Contador: {props.numero1}{props.numero2}{props.numero3}{props.numero4}{props.numero5}{props.numero6}</h1>
+			
+		</>
 	);
 };
 
--- src/js/index.js
+++ src/js/index.js
@@ -8,5 +8,51 @@ import "../styles/index.css";
 //import your own components
 import Home from "./component/home.jsx";
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let num1 = [0, 0, 0, 0, 0, 0];
+let check = true;
+setInterval(function() {
+            num1[5]++;
+            if (num1[5] > 9) {
+                num1[4]++;
+                num1[5] = 0;
+            }
+            if (num1[4] > 9) {
+                num1[3]++;
+                num1[4] = 0
+            }
+            if (num1[3] > 9) {
+                num1[2]++;
+                num1[3] = 0
+            }
+            if (num1[2] > 9) {
+                num1[1]++;
+                num1[2] = 0
+            }
+            if (num1[1] > 9) {
+                num1[0]++;
+                num1[1] = 0
+            }
+            const stopTimer = () => {
+                check = false
+            };
+            //render your react application
+            ReactDOM.render( < Home numero1 = {
+                    num1[0]
+                }
+                numero2 = {
+                    num1[1]
+                }
+                numero3 = {
+                    num1[2]
+                }
+                numero4 = {
+                    num1[3]
+                }
+                numero5 = {
+                    num1[4]
+                }
+                numero6 = {
+                    num1[5]
+                }
+                />, document.querySelector("#app"));
+            }, 500);
